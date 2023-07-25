2fd73e5ba467a6f8f5ee2831a2d6d3a54f26b34e
Frankie <frankie@episodemaker.com>
2023-07-25 15:30:08 -0400

    Updates

---
+++ _next/static/chunks/app/movie/page-1beeceaa6fdbac0a.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[133],{226:function(n,e,u){Promise.resolve().then(u.t.bind(u,8707,23))}},function(n){n.O(0,[707,667,488,744],function(){return n(n.s=226)}),_N_E=n.O()}]);
